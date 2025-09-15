a623ff54b81d76ec8a0a309559dfba6d0ce4a34f
Eugene Mutavchi <Ievgen_Mutavchi@comcast.com>
2025-09-15 13:24:40 +0000

    [GStreamer] fix buffer leak when capturing from camera device
    data probe shouldn't increase buffer ref count

--- Source/WebCore/platform/mediastream/gstreamer/GStreamerCapturer.cpp
+++ Source/WebCore/platform/mediastream/gstreamer/GStreamerCapturer.cpp
@@ -163,7 +163,7 @@ GstElement* GStreamerCapturer::createSource()
         gst_pad_add_probe(srcPad.get(), static_cast<GstPadProbeType>(GST_PAD_PROBE_TYPE_PUSH | GST_PAD_PROBE_TYPE_BUFFER), [](GstPad*, GstPadProbeInfo* info, gpointer) -> GstPadProbeReturn {
             VideoFrameTimeMetadata metadata;
             metadata.captureTime = MonotonicTime::now().secondsSinceEpoch();
-            auto modifiedBuffer = webkitGstBufferSetVideoFrameTimeMetadata(GRefPtr(GST_PAD_PROBE_INFO_BUFFER(info)), metadata);
+            auto modifiedBuffer = webkitGstBufferSetVideoFrameTimeMetadata(adoptGRef(GST_PAD_PROBE_INFO_BUFFER(info)), metadata);
             GST_PAD_PROBE_INFO_DATA(info) = modifiedBuffer.leakRef();
             return GST_PAD_PROBE_OK;
         }, nullptr, nullptr);
