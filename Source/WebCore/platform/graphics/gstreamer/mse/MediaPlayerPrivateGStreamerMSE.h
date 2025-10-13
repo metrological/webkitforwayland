15b9b55c61d37a3cab3c38c79b9168e7a98c9e6c
Enrique Ocaña González <eocanha@igalia.com>
2025-10-13 11:47:10 +0200

    [MSE][GStreamer] Early reset m_isEndReached (EOS) when about to seek
    When a video has reached End Of Stream and a (backwards) seek is done,
    the media player won't ever complete the seek because the EOS condition
    is still in place and no buffers are ever going to be pushed. A
    successful seek completion would reset the EOS condition, but that won't
    happen since the seek won't even start.
    
    This change resets m_isEndReached as soon as possible (on
    willSeekToTarget()), so the seek can happen.
    
    See: https://github.com/WebPlatformForEmbedded/WPEWebKit/pull/1568#issuecomment-3448502697

--- Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.h
+++ Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.h
@@ -54,6 +54,7 @@ public:
 
     void play() override;
     void pause() override;
+    void willSeekToTarget(const MediaTime& time) override;
     void seekToTarget(const SeekTarget&) override;
     bool doSeek(const SeekTarget&, float rate) override;
 
