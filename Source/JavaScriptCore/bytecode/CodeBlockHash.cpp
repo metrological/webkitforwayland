yoavweiss/WebKit

JSC: Replace UChar with char16_t https://bugs.webkit.org/show_bug.cgi?id=294724 rdar://153818709
Reviewed by Alex Christensen.

WebKit wishes to move from UChar to the C++ standard char16_t; they are
currently typedef'ed to be the same. This PR makes the textual substitutions
across JavaScriptCore. This is a simple search and replace for \bUChar\b with
no other changes, and it should have no functional effect.

This is one of a series of PRs for different parts of WebKit.

* Source/JavaScriptCore/API/JSStringRef.cpp:
(JSStringCreateWithCharacters):
(JSStringCreateWithUTF8CString):
(JSStringCreateWithCharactersNoCopy):
* Source/JavaScriptCore/API/JSStringRefCF.cpp:
(JSStringCreateWithCFString):
* Source/JavaScriptCore/API/JSValueRef.cpp:
(JSValueMakeFromJSONString):
* Source/JavaScriptCore/API/OpaqueJSString.cpp:
(OpaqueJSString::~OpaqueJSString):
(OpaqueJSString::characters):
* Source/JavaScriptCore/API/OpaqueJSString.h:
(OpaqueJSString::create):
(OpaqueJSString::OpaqueJSString):
* Source/JavaScriptCore/API/glib/JSCValue.cpp:
(jsc_value_new_from_json):
* Source/JavaScriptCore/API/tests/JSONParseTest.cpp:
(testJSONParse):
* Source/JavaScriptCore/KeywordLookupGenerator.py:
(Trie.printAsC):
* Source/JavaScriptCore/builtins/BuiltinNames.cpp:
(JSC::BuiltinNames::lookUpPrivateName const):
(JSC::BuiltinNames::lookUpWellKnownSymbol const):
* Source/JavaScriptCore/builtins/BuiltinNames.h:
* Source/JavaScriptCore/bytecode/CodeBlockHash.cpp:
(JSC::CodeBlockHash::CodeBlockHash):
* Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h:
(JSC::DFG::AbstractInterpreter<AbstractStateType>::executeEffects):
* Source/JavaScriptCore/dfg/DFGLazyJSValue.cpp:
(JSC::DFG::equalToSingleCharacter):
* Source/JavaScriptCore/dfg/DFGLazyJSValue.h:
(JSC::DFG::LazyJSValue::singleCharacterString):
(JSC::DFG::LazyJSValue::character const):
* Source/JavaScriptCore/dfg/DFGOperations.cpp:
(JSC::DFG::JSC_DEFINE_JIT_OPERATION):
* Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp:
* Source/JavaScriptCore

Source/JavaScriptCore/bytecode/CodeBlockHash.cpp
@@ -63,7 +63,7 @@ CodeBlockHash::CodeBlockHash(StringView codeBlockSourceCode, StringView entireSo
 
         sha1.addBytes(std::span { std::bit_cast<uint8_t*>(&length), sizeof(length) });
         do {
-            UChar character = entireSourceCode[index];
+            char16_t character = entireSourceCode[index];
             sha1.addBytes(std::span { std::bit_cast<uint8_t*>(&character), sizeof(character) });
             oldIndex = index;
             index += step;

metrological/webkitforwayland

Source/JavaScriptCore/bytecode/CodeBlockHash.cpp
@@ -66,7 +66,7 @@ CodeBlockHash::CodeBlockHash(const SourceCode& sourceCode, CodeSpecializationKin
 
         sha1.addBytes(std::span { std::bit_cast<uint8_t*>(&length), sizeof(length) });
         do {
-            UChar character = str[index];
+            char16_t character = str[index];
             sha1.addBytes(std::span { std::bit_cast<uint8_t*>(&character), sizeof(character) });
             oldIndex = index;
             index += step;