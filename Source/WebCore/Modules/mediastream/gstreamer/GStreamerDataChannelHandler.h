0458f8c1ed394c21ae4f311932749645129a4ec2
Philippe Normand <philn@igalia.com>
2026-02-27 05:42:23 -0800

    Cherry-pick 308333@main (74e1228c432c). https://bugs.webkit.org/show_bug.cgi?id=308364
        [GStreamer] imported/w3c/web-platform-tests/webrtc/protocol/dtls-fingerprint-validation.html is a flaky crash
        https://bugs.webkit.org/show_bug.cgi?id=308364
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        Wrap the handler reference in a DataChannelNotifier structure, passed to the GstWebRTC DataChannel
        GObject signal handlers. The crash was most likely due to the handler being destructed shortly
        before the bufferedAmountChanged method call.
    
        * Source/WebCore/Modules/mediastream/gstreamer/GStreamerDataChannelHandler.cpp:
        (WebCore::DataChannelNotifier::DataChannelNotifier):
        (WebCore::DataChannelNotifier::destruct):
        (WebCore::GStreamerDataChannelHandler::GStreamerDataChannelHandler):
        (WebCore::GStreamerDataChannelHandler::bufferedAmountChanged):
        * Source/WebCore/Modules/mediastream/gstreamer/GStreamerDataChannelHandler.h:
    
        Canonical link: https://commits.webkit.org/308333@main
    
    Canonical link: https://commits.webkit.org/298234.436@webkitglib/2.50

--- Source/WebCore/Modules/mediastream/gstreamer/GStreamerDataChannelHandler.h
+++ Source/WebCore/Modules/mediastream/gstreamer/GStreamerDataChannelHandler.h
@@ -64,9 +64,9 @@ private:
     void onMessageString(const char*);
     void onError(GError*);
     void onClose();
-
     void readyStateChanged();
-    void bufferedAmountChanged();
+    void bufferedAmountChanged(size_t);
+
     bool checkState();
     void postTask(Function<void()>&&);
 
@@ -87,6 +87,8 @@ private:
     bool m_closing { false };
 
     String m_channelId;
+
+    Vector<unsigned long, 6> m_signalHandlers;
 };
 
 } // namespace WebCore
