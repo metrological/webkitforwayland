tomasz-karczewski-red/WPEWebKit

Add configuration option to allow moving window to background when window.close is called
When allow_scripts_to_close_windows property is set to true, scripts can
close windows that are not opened by them. But after window.close API is
called, page is in "is closing" and browser expects window to be closed.
When integrator wants to hide browser window/suspend browser, instead of
closing it, page will be stuck in "is closing" state, where some APIs
don't work anymore.

This change introduces new property allow_move_to_suspend_on_window_close
(disabled by default) which removes setting "is closing" state and just
sends notification to browser integration, without any expectations on
how it's handled.

Source/WebCore/page/DOMWindow.cpp
@@ -1073,17 +1073,19 @@ void DOMWindow::close()
     if (!frame->isMainFrame())
         return;
 
-    if (!(page->openedByDOM() || page->backForward().count() <= 1 || frame->settings().allowScriptsToCloseWindows())) {
-        console()->addMessage(MessageSource::JS, MessageLevel::Warning, "Can't close the window since it was not opened by JavaScript"_s);
-        return;
+    if (!frame->settings().allowMoveToSuspendOnWindowClose()) {
+        if (!(page->openedByDOM() || page->backForward().count() <= 1 || frame->settings().allowScriptsToCloseWindows())) {
+            console()->addMessage(MessageSource::JS, MessageLevel::Warning, "Can't close the window since it was not opened by JavaScript"_s);
+            return;
+        }
+
+        if (!frame->loader().shouldClose())
+            return;
+
+        ResourceLoadObserver::shared().updateCentralStatisticsStore([] { });
+
+        page->setIsClosing();
     }
-
-    if (!frame->loader().shouldClose())
-        return;
-
-    ResourceLoadObserver::shared().updateCentralStatisticsStore([] { });
-
-    page->setIsClosing();
 
     document()->eventLoop().queueTask(TaskSource::DOMManipulation, [this, protectedThis = Ref { *this }] {
         if (auto* page = this->page())

metrological/webkitforwayland

Source/WebCore/page/DOMWindow.cpp
@@ -117,18 +117,20 @@ void DOMWindow::close()
     if (!frame->isMainFrame())
         return;
 
-    if (!(page->openedByDOM() || page->checkedBackForward()->count() <= 1 || frame->settings().allowScriptsToCloseWindows())) {
-        checkedConsole()->addMessage(MessageSource::JS, MessageLevel::Warning, "Can't close the window since it was not opened by JavaScript"_s);
-        return;
+    if (!frame->settings().allowMoveToSuspendOnWindowClose()) {
+        if (!(page->openedByDOM() || page->checkedBackForward()->count() <= 1 || frame->settings().allowScriptsToCloseWindows())) {
+            checkedConsole()->addMessage(MessageSource::JS, MessageLevel::Warning, "Can't close the window since it was not opened by JavaScript"_s);
+            return;
+        }
+
+        RefPtr localFrame = dynamicDowncast<LocalFrame>(frame);
+        if (localFrame && !localFrame->loader().shouldClose())
+            return;
+
+        ResourceLoadObserver::shared().updateCentralStatisticsStore([] { });
+
+        page->setIsClosing();
     }
-
-    RefPtr localFrame = dynamicDowncast<LocalFrame>(frame);
-    if (localFrame && !localFrame->loader().shouldClose())
-        return;
-
-    ResourceLoadObserver::shared().updateCentralStatisticsStore([] { });
-
-    page->setIsClosing();
     closePage();
 }
 