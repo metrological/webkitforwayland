tomasz-karczewski-red/WPEWebKit

Jettison on top level navigation
This fix is ported from:
https://github.com/WebPlatformForEmbedded/WPEWebKit/commit/6481564a48195de05a1778c46bf9f81aead82d0f

Source/WebCore/page/MemoryRelease.cpp
@@ -234,7 +234,24 @@ void logMemoryStatistics(LogMemoryStatisticsReason reason)
 #if !PLATFORM(COCOA)
 void platformReleaseMemory(Critical) { }
 void platformReleaseGraphicsMemory(Critical) { }
-void jettisonExpensiveObjectsOnTopLevelNavigation() { }
+void jettisonExpensiveObjectsOnTopLevelNavigation()
+{
+    // based on code from cocoa/MemoryReleaseCocoa.mm
+    // Protect against doing excessive jettisoning during repeated navigations.
+    const auto minimumTimeSinceNavigation = std::chrono::seconds(2);
+
+    const auto now = std::chrono::steady_clock::now();
+    static auto timeOfLastNavigation = now;
+    const bool shouldJettison = (timeOfLastNavigation == now) || (std::chrono::duration_cast<std::chrono::seconds>(now - timeOfLastNavigation) >= minimumTimeSinceNavigation);
+    timeOfLastNavigation = now;
+
+    if (!shouldJettison)
+        return;
+
+    RunLoop::main().dispatch([]{
+        releaseMemory(Critical::Yes, Synchronous::Yes);
+    });
+}
 void registerMemoryReleaseNotifyCallbacks() { }
 #endif
 

metrological/webkitforwayland

Source/WebCore/page/MemoryRelease.cpp
@@ -276,7 +276,24 @@ void logMemoryStatistics(LogMemoryStatisticsReason reason)
 void platformReleaseMemory(Critical) { }
 #endif
 void platformReleaseGraphicsMemory(Critical) { }
-void jettisonExpensiveObjectsOnTopLevelNavigation() { }
+void jettisonExpensiveObjectsOnTopLevelNavigation()
+{
+    // based on code from cocoa/MemoryReleaseCocoa.mm
+    // Protect against doing excessive jettisoning during repeated navigations.
+    const auto minimumTimeSinceNavigation = std::chrono::seconds(2);
+
+    const auto now = std::chrono::steady_clock::now();
+    static auto timeOfLastNavigation = now;
+    const bool shouldJettison = (timeOfLastNavigation == now) || (std::chrono::duration_cast<std::chrono::seconds>(now - timeOfLastNavigation) >= minimumTimeSinceNavigation);
+    timeOfLastNavigation = now;
+
+    if (!shouldJettison)
+        return;
+
+    RunLoop::mainSingleton().dispatch([]{
+        releaseMemory(Critical::Yes, Synchronous::Yes);
+    });
+}
 void registerMemoryReleaseNotifyCallbacks() { }
 #endif
 