eugeneia/WebKit

Add API to manually check WebProcess responsiveness

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -9965,6 +9965,21 @@ void WebPageProxy::didChangeProcessIsResponsive()
     internals().pageLoadState.didChangeProcessIsResponsive();
 }
 
+void WebPageProxy::isWebProcessResponsive(CompletionHandler<void (bool)>&& callback)
+{
+    if (m_isClosed) {
+        if (callback) {
+            RunLoop::main().dispatch([callback = WTFMove(callback)]() mutable {
+                bool isWebProcessResponsive = true;
+                callback(isWebProcessResponsive);
+            });
+        }
+        return;
+    }
+
+    legacyMainFrameProcess().isResponsive(WTFMove(callback));
+}
+
 String WebPageProxy::currentURL() const
 {
     String url = internals().pageLoadState.activeURL();

metrological/webkitforwayland

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -11317,6 +11317,21 @@ void WebPageProxy::didChangeProcessIsResponsive()
     protectedPageLoadState()->didChangeProcessIsResponsive();
 }
 
+void WebPageProxy::isWebProcessResponsive(CompletionHandler<void (bool)>&& callback)
+{
+    if (m_isClosed) {
+        if (callback) {
+            RunLoop::mainSingleton().dispatch([callback = WTFMove(callback)]() mutable {
+                bool isWebProcessResponsive = true;
+                callback(isWebProcessResponsive);
+            });
+        }
+        return;
+    }
+
+    legacyMainFrameProcess().isResponsive(WTFMove(callback));
+}
+
 String WebPageProxy::currentURL() const
 {
     String url = protectedPageLoadState()->activeURL();