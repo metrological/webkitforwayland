dc209c8403c5b052eb21a911bf5feae8c2f52db9
Angelos Oikonomopoulos <angelos@igalia.com>
2026-05-07 14:48:47 +0200

    Revert "InById / InByVal should reset SP too in Baseline https://bugs.webkit.org/show_bug.cgi?id=276688 rdar://131869701"
    This reverts commit d1b5c9f2c385834bab36af4ca8990718a633c644.

--- Source/JavaScriptCore/jit/JITPropertyAccess.cpp
+++ Source/JavaScriptCore/jit/JITPropertyAccess.cpp
@@ -1273,7 +1273,6 @@ void JIT::emit_op_in_by_id(const JSInstruction* currentInstruction)
     addSlowCase();
     m_inByIds.append(gen);
 
-    resetSP(); // We might OSR exit here, so we need to conservatively reset SP
     setFastPathResumePoint();
     emitPutVirtualRegister(resultVReg, resultJSR);
 }
@@ -1334,7 +1333,6 @@ void JIT::emit_op_in_by_val(const JSInstruction* currentInstruction)
     addSlowCase();
     m_inByVals.append(gen);
 
-    resetSP(); // We might OSR exit here, so we need to conservatively reset SP
     setFastPathResumePoint();
     emitPutVirtualRegister(dst, resultJSR);
 }
@@ -1387,7 +1385,6 @@ void JIT::emitHasPrivate(VirtualRegister dst, VirtualRegister base, VirtualRegis
     addSlowCase();
     m_inByVals.append(gen);
 
-    resetSP(); // We might OSR exit here, so we need to conservatively reset SP
     setFastPathResumePoint();
     emitPutVirtualRegister(dst, resultJSR);
 }
