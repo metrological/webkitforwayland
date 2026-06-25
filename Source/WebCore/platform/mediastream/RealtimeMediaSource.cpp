yoavweiss/WebKit

[libWebRTC][GStreamer] Missing video in WebRTC playback when decoding on playback pipeline https://bugs.webkit.org/show_bug.cgi?id=317111
Reviewed by Xabier Rodriguez-Calvar.

LibWebRTC has a dedicated Decoder component that is supposed to decode video
frames and supply them like that to its observers. Under those assumptions,
missing some of those decoded frames (eg: because no observer is listening yet)
isn't very problematic in this expected case.

However, in order to facilitate hardware decoding, the libWebRTC Decoder
implementation provided by the GStreamer port emits encoded frames (that is,
doesn't decode at all) unders some circumstances, with the hope that those
encoded frames end up reaching the playback pipeline and being decoded there.

In such a case, missing video frames because no observer has been registered
(a circumstance that has been reported by at least one downstream setup but
that can't be reproduced reliably) can be problematic. Missing the first
"frames" (buffers) of an encoded video means that the video headers (eg:
SPS/PPS for H.264) are missed that's a condition the actual video decoder
can't recover from.

This patch provides a mechanism to keep undelivered buffers when they come
in an encoded format and there's no registered observer to deliver them to,
and redeliver them when the first observer is registered. This solves the
problem detected in the mentioned downstream setup.

If a format change is detected or if RealtimeMediaSource is stopping, the
list of pending frames is emtied, since there's no point anymore on
delivering them.

Co-authored by: Ganesh prasad Sahu <GaneshPrasad_Sahu@comcast.com>

* Source/WebCore/platform/VideoFrame.h:
(WebCore::VideoFrame::isEncoded const): Added generic implementation to know if a VideoFrame has an encoded format or not. Defaults to false.
(WebCore::VideoFrame::hasSameEncodedFormat const): Compares if format of one sample is the same as the one from another. Defaults to false.
* Source/WebCore/p

Source/WebCore/platform/mediastream/RealtimeMediaSource.cpp
@@ -362,32 +362,60 @@ void RealtimeMediaSource::videoFrameAvailable(VideoFrame& videoFrame, VideoFrame
 #endif
     }();
 
-    for (auto& [key, value] : m_videoFrameObservers) {
-        if (auto* adaptor = value.get()) {
-            if (adaptor->frameDecimation > 1 && ++adaptor->frameDecimationCounter % adaptor->frameDecimation)
-                continue;
+    // videoFrameObservers needs to be passed as parameter because if m_videoFrameObservers
+    // is used, that confuses the thread safety static analysis, which can't deduce that
+    // the lock has been taken by the outer scope of the videoFrameAvailable() method.
+    auto deliverFrame = [&] (Ref<VideoFrame>&& frame, VideoFrameTimeMetadata&& frameMetadata,
+        HashMap<VideoFrameObserver*, std::unique_ptr<VideoFrameAdaptor>>& videoFrameObservers) {
+        for (auto& [key, value] : videoFrameObservers) {
+            if (auto* adaptor = value.get()) {
+                if (adaptor->frameDecimation > 1 && ++adaptor->frameDecimationCounter % adaptor->frameDecimation)
+                    continue;
 
-            adaptor->frameDecimation = adaptor->frameRate ? static_cast<size_t>(observedFrameRate() / adaptor->frameRate) : 1;
-            if (!adaptor->frameDecimation)
-                adaptor->frameDecimation = 1;
+                adaptor->frameDecimation = adaptor->frameRate ? static_cast<size_t>(observedFrameRate() / adaptor->frameRate) : 1;
+                if (!adaptor->frameDecimation)
+                    adaptor->frameDecimation = 1;
 
-            if (!adaptor->size.isZero()) {
-                auto actualSize = expandedIntSize(currentVideoFrame->presentationSize());
-                auto adaptorSize = adaptor->size;
-                if (shouldSwapAdaptorSize)
-                    adaptorSize = adaptorSize.transposedSize();
-                auto desiredSize = computeResizedVideoFrameSize(adaptorSize, actualSize);
+                if (!adaptor->size.isZero()) {
+                    auto actualSize = expandedIntSize(frame->presentationSize());
+                    auto adaptorSize = adaptor->size;
+                    if (shouldSwapAdaptorSize)
+                        adaptorSize = adaptorSize.transposedSize();
+                    auto desiredSize = computeResizedVideoFrameSize(adaptorSize, actualSize);
 
-                if (desiredSize != actualSize) {
-                    if (auto newVideoFrame = adaptVideoFrame(*adaptor, currentVideoFrame, desiredSize)) {
-                        key->videoFrameAvailable(*newVideoFrame, metadata);
-                        continue;
+                    if (desiredSize != actualSize) {
+                        if (auto newVideoFrame = adaptVideoFrame(*adaptor, frame, desiredSize)) {
+                            key->videoFrameAvailable(*newVideoFrame, frameMetadata);
+                            continue;
+                        }
                     }
                 }
             }
+            key->videoFrameAvailable(frame, frameMetadata);
         }
-        key->videoFrameAvailable(currentVideoFrame, metadata);
+    };
+
+    // Store encoded (H.264, vp9, etc.) video frames to avoid missing the headers when the first observer starts listening.
+    if (m_videoFrameObservers.isEmpty() && currentVideoFrame->isEncoded()) {
+        // If the encoded format has changed, empty the pending frames, because there's no point on delivering them anymore.
+        if (!m_pendingVideoFrames.isEmpty() && !(protect(m_pendingVideoFrames.last().frame)->hasSameEncodedFormat(currentVideoFrame)))
+            m_pendingVideoFrames.clear();
+
+        if (m_pendingVideoFrames.size() < maxPendingVideoFramesBeforeAddTrack)
+            m_pendingVideoFrames.append(PendingVideoFrame { WTF::move(currentVideoFrame), WTF::move(metadata) });
+        else
+            ALWAYS_LOG_IF(m_logger, LOGIDENTIFIER, "RealtimeMediaSource: Dropping video frame (queue is full) ", m_pendingVideoFrames.size(), " frames");
+        return;
     }
+
+    if (!m_pendingVideoFrames.isEmpty()) {
+        ALWAYS_LOG_IF(m_logger, LOGIDENTIFIER, "RealtimeMediaSource: Delivering ", m_pendingVideoFrames.size(), " queued frame(s) (observer ready)");
+        for (auto& pending : m_pendingVideoFrames)
+            deliverFrame(WTF::move(pending.frame), WTF::move(pending.metadata), m_videoFrameObservers);
+        m_pendingVideoFrames.clear();
+    }
+
+    deliverFrame(WTF::move(currentVideoFrame), WTF::move(metadata), m_videoFrameObservers);
 }
 
 void RealtimeMediaSource::audioSamplesAvailable(const MediaTime& time, const PlatformAudioData& audioData, const AudioStreamDescription& description, size_t numberOfFrames)
@@ -428,6 +456,12 @@ void RealtimeMediaSource::stop()
     ALWAYS_LOG_IF(m_logger, LOGIDENTIFIER);
 
     m_isProducingData = false;
+
+    {
+        Locker locker { m_videoFrameObserversLock };
+        m_pendingVideoFrames.clear();
+    }
+
     stopProducingData();
 }
 

metrological/webkitforwayland

Source/WebCore/platform/mediastream/RealtimeMediaSource.cpp
@@ -339,29 +339,58 @@ void RealtimeMediaSource::videoFrameAvailable(VideoFrame& videoFrame, VideoFrame
     updateHasStartedProducingData();
 
     Locker locker { m_videoFrameObserversLock };
-    for (auto& [key, value] : m_videoFrameObservers) {
-        if (auto* adaptor = value.get()) {
-            if (adaptor->frameDecimation > 1 && ++adaptor->frameDecimationCounter % adaptor->frameDecimation)
-                continue;
 
-            adaptor->frameDecimation = adaptor->frameRate ? static_cast<size_t>(observedFrameRate() / adaptor->frameRate) : 1;
-            if (!adaptor->frameDecimation)
-                adaptor->frameDecimation = 1;
+    // videoFrameObservers needs to be passed as parameter because if m_videoFrameObservers
+    // is used, that confuses the thread safety static analysis, which can't deduce that
+    // the lock has been taken by the outer scope of the videoFrameAvailable() method.
+    auto deliverFrame = [&] (Ref<VideoFrame>&& frame, VideoFrameTimeMetadata&& frameMetadata,
+        HashMap<VideoFrameObserver*, std::unique_ptr<VideoFrameAdaptor>>& videoFrameObservers) {
+        for (auto& [key, value] : videoFrameObservers) {
+            if (auto* adaptor = value.get()) {
+                if (adaptor->frameDecimation > 1 && ++adaptor->frameDecimationCounter % adaptor->frameDecimation)
+                    continue;
 
-            if (!adaptor->size.isZero()) {
-                auto actualSize = expandedIntSize(videoFrame.presentationSize());
-                auto desiredSize = computeResizedVideoFrameSize(adaptor->size, actualSize);
+                adaptor->frameDecimation = adaptor->frameRate ? static_cast<size_t>(observedFrameRate() / adaptor->frameRate) : 1;
+                if (!adaptor->frameDecimation)
+                    adaptor->frameDecimation = 1;
 
-                if (desiredSize != actualSize) {
-                    if (auto newVideoFrame = adaptVideoFrame(*adaptor, videoFrame, desiredSize)) {
-                        key->videoFrameAvailable(*newVideoFrame, metadata);
-                        continue;
+                if (!adaptor->size.isZero()) {
+                    auto actualSize = expandedIntSize(frame->presentationSize());
+                    auto desiredSize = computeResizedVideoFrameSize(adaptor->size, actualSize);
+
+                    if (desiredSize != actualSize) {
+                        if (auto newVideoFrame = adaptVideoFrame(*adaptor, frame, desiredSize)) {
+                            key->videoFrameAvailable(*newVideoFrame, metadata);
+                            continue;
+                        }
                     }
                 }
             }
+            key->videoFrameAvailable(frame, metadata);
         }
-        key->videoFrameAvailable(videoFrame, metadata);
+    };
+
+    // Store encoded (H.264, vp9, etc.) video frames to avoid missing the headers when the first observer starts listening.
+    if (m_videoFrameObservers.isEmpty() && videoFrame.isEncoded()) {
+        // If the encoded format has changed, empty the pending frames, because there's no point on delivering them anymore.
+        if (!m_pendingVideoFrames.isEmpty() && !(Ref(m_pendingVideoFrames.last().frame)->hasSameEncodedFormat(videoFrame)))
+            m_pendingVideoFrames.clear();
+
+        if (m_pendingVideoFrames.size() < maxPendingVideoFramesBeforeAddTrack)
+            m_pendingVideoFrames.append(PendingVideoFrame { Ref(videoFrame), WTFMove(metadata) });
+        else
+            ALWAYS_LOG_IF(m_logger, LOGIDENTIFIER, "RealtimeMediaSource: Dropping video frame (queue is full) ", m_pendingVideoFrames.size(), " frames");
+        return;
     }
+
+    if (!m_pendingVideoFrames.isEmpty()) {
+        ALWAYS_LOG_IF(m_logger, LOGIDENTIFIER, "RealtimeMediaSource: Delivering ", m_pendingVideoFrames.size(), " queued frame(s) (observer ready)");
+        for (auto& pending : m_pendingVideoFrames)
+            deliverFrame(WTFMove(pending.frame), WTFMove(pending.metadata), m_videoFrameObservers);
+        m_pendingVideoFrames.clear();
+    }
+
+    deliverFrame(videoFrame, WTFMove(metadata), m_videoFrameObservers);
 }
 
 void RealtimeMediaSource::audioSamplesAvailable(const MediaTime& time, const PlatformAudioData& audioData, const AudioStreamDescription& description, size_t numberOfFrames)